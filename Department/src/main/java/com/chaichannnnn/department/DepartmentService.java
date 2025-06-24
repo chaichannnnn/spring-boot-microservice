096b0704b95c6c42052980937644f955db0e44df
Chanchai Nonthachai <40120332+chaichannnnn@users.noreply.github.com>
2025-06-24 23:19:50 +0700

    [Department-service][circuit braker] Enable annotation circuit braker

--- Department/src/main/java/com/chaichannnnn/department/DepartmentService.java
+++ Department/src/main/java/com/chaichannnnn/department/DepartmentService.java
@@ -2,7 +2,7 @@ package com.chaichannnnn.department;
 
 import com.chaichannnnn.department.employee.Employee;
 import com.chaichannnnn.department.employee.EmployeeClient;
-//import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,7 +17,7 @@ public class DepartmentService {
 
     private final EmployeeClient employeeClient;
 
-    //@CircuitBreaker(name = SAMPLE_SERVICE, fallbackMethod = "fallbackResponse")
+    @CircuitBreaker(name = SAMPLE_SERVICE, fallbackMethod = "fallbackResponse")
     public Department testFindDepartment() {
         Optional<Department> department = Optional.of(Department.builder().id("123").name("hello department").build());
         department.ifPresent(d -> {

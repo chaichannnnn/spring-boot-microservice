8b89bab5d72641cdcdf5d0424d580ac8e82e0e43
Chanchai Nonthachai <40120332+chaichannnnn@users.noreply.github.com>
2025-06-26 20:52:14 +0700

    [Employee] Remove MongoDb

--- Employee/src/main/java/com/chaichannnnn/employee/EmployeeRepository.java
+++ Employee/src/main/java/com/chaichannnnn/employee/EmployeeRepository.java
@@ -1,10 +1,10 @@
-package com.chaichannnnn.employee;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.List;
-
-public interface EmployeeRepository extends MongoRepository<Employee, String> {
-    List<Employee> findByDepartmentId(String departmentId);
-    List<Employee> findByOrganizationId(String organizationId);
-}
+//package com.chaichannnnn.employee;
+//
+//import org.springframework.data.mongodb.repository.MongoRepository;
+//
+//import java.util.List;
+//
+//public interface EmployeeRepository extends MongoRepository<Employee, String> {
+//    List<Employee> findByDepartmentId(String departmentId);
+//    List<Employee> findByOrganizationId(String organizationId);
+//}
